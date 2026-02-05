5b30121bab8bc1d2741cbf212f9b9b93cb98cea4
Marcus Rezende <marcusviniciusxmvr@gmail.com>
2026-02-05 18:09:46 -0300

    feat: iniciando página sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Footer from './componentes/Footer'
-
+import React from 'react';
+import Sobre from './componentes/Sobre';
 
 const App = () => {
 
   return (
     
-
     <div>
+      <Sobre />
       <Footer />    
     </div>
   )
