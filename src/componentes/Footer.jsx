bf0bb2b26a71b7c204fb37f9e83d5be48e4c2c28
Marcus Rezende <marcusviniciusxmvr@gmail.com>
2026-02-26 14:47:24 -0300

    todas as seçoes animadas com efeito init hidden

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,55 +1,69 @@
-import React from 'react';
-import logoFooter from '../assets/footer.png'
+import React from "react";
+import logoFooter from "../assets/footer.png";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { BsTelephone } from "react-icons/bs";
 import { MdOutlineEmail } from "react-icons/md";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 
-import '../styles/footer.css'
+import "../styles/footer.css";
 
+const Footer = () => {
+  return (
+    <footer className="rodape" id="contato">
+      <div className="rodape-conteudo">
+        <div className="init-hidden">
+          <a href="/">
+            <div className="foto-footer">
+              <img src={logoFooter} alt="logo footer" className="foto" />
+            </div>
+          </a>
+        </div>
+        <div className="bloco2">
+          <div className="secao-contatos">
+            <h1 className=" titulo-footer"> Contatos </h1>
 
-const Footer = ()  => {
-    return (
+            <p className="contatos">
+              {" "}
+              <BsTelephone className="icone" /> (61) 9xxxx-xxxx
+            </p>
+            <p className="contatos">
+              {" "}
+              <MdOutlineEmail className="icone" /> email@gmail.com
+            </p>
+            <p className="contatos">
+              {" "}
+              <HiOutlineLocationMarker className="icone" /> Brasilia - DF
+            </p>
+          </div>
 
-        <footer className='rodape' id='contato'>
-            <div className='rodape-conteudo'>
-                <a href="/">
-                    <div className='foto-footer'>
-                        <img src ={logoFooter} alt = "logo footer" className='foto' />
-                    </div>
+          <div className="secao-extras">
+            <p className="contatos-informacoes">
+              Transformando festas infatis em momentos mágicos e inesquecíveis
+            </p>
+            <div className="contatos-icones">
+              <div className="instagram">
+                <a href="https://www.instagram.com/recreya/">
+                  <FaInstagram className="social" />
                 </a>
-                <div className='bloco2'>
-                    <div className='secao-contatos'>
-                        
-                        <h1 className=' titulo-footer'> Contatos </h1>
-                        
-                        <p className='contatos'> <BsTelephone className='icone'/> (61) 9xxxx-xxxx</p>
-                        <p className='contatos'> <MdOutlineEmail className='icone'/> email@gmail.com</p>
-                        <p className='contatos'> <HiOutlineLocationMarker className='icone' /> Brasilia - DF</p>
-                    </div>
-
-                    <div className='secao-extras'>
-                        <p className='contatos-informacoes' >Transformando festas infatis em momentos mágicos e inesquecíveis</p>
-                        <div className='contatos-icones'>
-                            <div className='instagram'> 
-                                <a href="https://www.instagram.com/recreya/">
-                                <FaInstagram className='social' />
-                                </a>
-                            </div>
-                            <div className='whatsapp'> 
-                                <a href="https://www.instagram.com/recreya/">
-                                <FaWhatsapp className='social'/>
-                                </a>
-                            </div>
-                        </div>
-                        <p className='contatos-informacoes'>Representanet Legal: Yasmin Daniele Cassiano Vieira</p>
-                    </div>
-                </div>
+              </div>
+              <div className="whatsapp">
+                <a href="https://www.instagram.com/recreya/">
+                  <FaWhatsapp className="social" />
+                </a>
+              </div>
             </div>
-            <div className='seguranca'>
-                <p className='seguranca-frase' >© 2026 Recreya | Todos os direitos reservados.</p>
-            </div>
-        </footer>
-    );
-
-}; export default Footer;
+            <p className="contatos-informacoes">
+              Representanet Legal: Yasmin Daniele Cassiano Vieira
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="seguranca">
+        <p className="seguranca-frase">
+          © 2026 Recreya | Todos os direitos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- src/componentes/Portfolio.jsx
+++ src/componentes/Portfolio.jsx
@@ -14,68 +14,73 @@ function Portfolio() {
   return (
     <div className="portfolio-container">
       <div className="portfolio">
-        <div className="texto-portfolio-container">
-          <div className="icone-chapeu-container">
-            <img src={fotoIcone} alt="icone da empresa recreya" className="icone-chapeu" />
-          </div>
-          
-          <h2 className="titulo-portfolio">
-            Desde 20XX Atuando em festas para todas as famílias
-          </h2>
-          <p className="texto-portfolio">De Brasília a Goiânia </p>
-        </div>
-        <div className="imagens-portfolio-container">
-          <div className="imagem-portfolio-um">
-            <AiFillPushpin
-              className="icone-foto-um"
-              style={{ stroke: "black", strokeWidth: "25px" }}
-            />
+        <div className="init-hidden">
+          <div className="texto-portfolio-container">
+            <div className="icone-chapeu-container">
+              <img
+                src={fotoIcone}
+                alt="icone da empresa recreya"
+                className="icone-chapeu"
+              />
+            </div>
 
-            <img
-              src={foto1}
-              alt="foto da empresa recreya"
-              className="foto-portfolio"
-            />
+            <h2 className="titulo-portfolio">
+              Desde 20XX Atuando em festas para todas as famílias
+            </h2>
+            <p className="texto-portfolio">De Brasília a Goiânia </p>
           </div>
+          <div className="imagens-portfolio-container">
+            <div className="imagem-portfolio-um">
+              <AiFillPushpin
+                className="icone-foto-um"
+                style={{ stroke: "black", strokeWidth: "25px" }}
+              />
 
-          <div className="imagem-portfolio-dois">
-            <AiFillPushpin
-              className="icone-foto-dois"
-              style={{ stroke: "black", strokeWidth: "25px" }}
-            />
-            <img
-              src={foto4}
-              alt="foto da empresa recreya"
-              className="foto-portfolio"
-            />
-          </div>
+              <img
+                src={foto1}
+                alt="foto da empresa recreya"
+                className="foto-portfolio"
+              />
+            </div>
 
-          <div className="imagem-portfolio-tres">
-            <AiFillPushpin
-              className="icone-foto-tres"
-              style={{ stroke: "black", strokeWidth: "25px" }}
-            />
-            <img
-              src={foto2}
-              alt="foto da empresa recreya"
-              className="foto-portfolio"
-            />
-          </div>
+            <div className="imagem-portfolio-dois">
+              <AiFillPushpin
+                className="icone-foto-dois"
+                style={{ stroke: "black", strokeWidth: "25px" }}
+              />
+              <img
+                src={foto4}
+                alt="foto da empresa recreya"
+                className="foto-portfolio"
+              />
+            </div>
 
-          <div className="imagem-portfolio-quatro">
-            <AiFillPushpin
-              className="icone-foto-quatro"
-              style={{ stroke: "black", strokeWidth: "25px" }}
-            />
-            <img
-              src={foto3}
-              alt="foto da empresa recreya"
-              className="foto-portfolio"
-            />
+            <div className="imagem-portfolio-tres">
+              <AiFillPushpin
+                className="icone-foto-tres"
+                style={{ stroke: "black", strokeWidth: "25px" }}
+              />
+              <img
+                src={foto2}
+                alt="foto da empresa recreya"
+                className="foto-portfolio"
+              />
+            </div>
+
+            <div className="imagem-portfolio-quatro">
+              <AiFillPushpin
+                className="icone-foto-quatro"
+                style={{ stroke: "black", strokeWidth: "25px" }}
+              />
+              <img
+                src={foto3}
+                alt="foto da empresa recreya"
+                className="foto-portfolio"
+              />
+            </div>
           </div>
         </div>
       </div>
-      <div className="icones"></div>
     </div>
   );
 }
